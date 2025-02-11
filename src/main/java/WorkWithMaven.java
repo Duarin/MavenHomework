b90d3aa50d0d6a8af2b188371d1cbde080698ed0
Duarin <bilvania2906@gmail.com>
2025-02-11 20:15:51 +0100

    work with maven

---
+++ src/main/java/WorkWithMaven.java
@@ -0,0 +1,8 @@
+import bil.vania.ArrayGenerated.PasswordGenerator;
+public class WorkWithMaven {
+    public static void main(String[] args) {
+        String password = PasswordGenerator.generatePassword(12);
+        System.out.println("Generated password: " + password);
+
+    }
+}
